8b3f7c4dcab80299e05c59a96fd62033b43277e9
kenanmutuma <kenanmutuma12@gmail.com>
2023-03-17 12:45:42 -0700

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -4,5 +4,3 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
-
-print()      # Add this line to print a newline character at the end
